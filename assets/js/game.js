2076266244faa73669ff6a6258cef1d04cb54cd6
Ian Miller <imiller142@gmail.com>
2021-09-07 16:31:35 -0500

    initial variable added

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+// Robot name
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+// Creation of fight function
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
